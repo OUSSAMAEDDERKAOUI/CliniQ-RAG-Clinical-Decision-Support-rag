b3375564aa6eb9d7c79f21c3828b1ca7fc1f0c03
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2026-02-24 11:38:29 +0000

    feat(auth): add password hashing and JWT access token generation

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    embedding_model: str = "all-MiniLM-L6-v2"
+    vector_db: str = "chroma"
+    persist_directory: str = "./vectorstore"
+
+    CHROMA_HOST: str
+    OLLAMA_URL: str
+    DATA_DIR: str
+    ENABLE_EVALUATION: bool = True   
+
+    class Config:
+        env_file = ".env"
+        extra = "allow"   
+
+
+settings = Settings()
